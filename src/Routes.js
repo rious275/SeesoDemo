5a699484b26ccf417d0e55e692d3cc9bf78cbfb8
rious275 <rious275@naver.com>
2021-09-16 13:16:02 +0900

    Add: 드래그 기능을 제외한 구현 완료

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // 페이지 구분
-import Main from './pages/Start';
+import Start from './pages/Start';
 import Setting from './pages/Setting.js';
 import Demo from './pages/Demo';
 
@@ -10,7 +10,7 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={Start} />
         <Route exact path="/setting" component={Setting} />
         <Route exact path="/demo" component={Demo} />
       </Switch>
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+
+import { useHistory } from 'react-router-dom';
+
+import { BROCHURE_URL } from '../Variable';
 
 const Start = () => {
-  return <div>Main</div>;
+  const history = useHistory();
+
+  const handleNext = () => {
+    history.push('/setting');
+  };
+
+  return (
+    <Container>
+      <WebcamIcon alt="" src="images/icon_webcam.svg" dragggable="true" />
+      <GuideText>상단의 아이콘을 드래그해서 웹캠 위치를 알려주세요.</GuideText>
+      <NextBtn onClick={handleNext}>NEXT</NextBtn>
+      <BrochureLink href={BROCHURE_URL} target="_blank">
+        좀 더 자세한 설명히 필요하세요?
+      </BrochureLink>
+    </Container>
+  );
 };
 
 export default Start;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100vh;
+`;
+
+const WebcamIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  width: 50px;
+`;
+
+const GuideText = styled.p`
+  margin-bottom: 60px;
+`;
+
+const NextBtn = styled.button`
+  display: flex;
+  margin-bottom: 20px;
+  padding: 16px 38px;
+
+  border-style: none;
+  border-radius: 28px;
+  color: #fff;
+  background-color: #4e5fbb;
+  font-size: 18px;
+  font-weight: 600;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const BrochureLink = styled.a`
+  &:visited {
+    color: #000;
+  }
+`;
